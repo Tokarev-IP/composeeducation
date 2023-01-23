3bec63370bcdcf50cd9dead1383ef7c099feac2f
TokarevIP <tokarevrab@gmail.com>
2023-01-23 22:31:51 +0000

    google compose education part 2

--- app/src/main/java/test/app/composetestapp/MainActivity.kt
+++ app/src/main/java/test/app/composetestapp/MainActivity.kt
@@ -1,44 +1,71 @@
 package test.app.composetestapp
 
-import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import test.app.composetestapp.ui.theme.ComposeTestAppTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.saveable.rememberSaveable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestAppTheme {
-                MyApp(Modifier.fillMaxSize())
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-private fun MyApp(
+private fun OnBoardingScreen(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
-) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name)
+    buttonCallBack:() -> Unit,
+){
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Text(text = "Welcome to the Basics Codelab!")
+        Button(onClick = { buttonCallBack() }) {
+            Text(text = "Continue")
+        }
+    }
+
+}
+
+@Composable
+private fun MyApp() {
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
+
+    if (shouldShowOnBoarding)
+        OnBoardingScreen(buttonCallBack = { shouldShowOnBoarding = false })
+    else { Greetings() }
+}
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" },
+){
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names){name ->
+            Greeting(name = name)
         }
     }
 }
@@ -49,28 +76,44 @@ fun Greeting(name: String) {
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        val extended = remember { mutableStateOf(false) }
-        val extraPadding = if (extended.value) 48.dp else 0.dp
+        var extended by remember { mutableStateOf(false) }
+
+        val extraPadding by animateDpAsState(
+            if (extended) 48.dp else 0.dp,
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioHighBouncy,
+                stiffness = Spring.StiffnessVeryLow,
+            )
+        )
 
         Row(Modifier.padding(24.dp)) {
-            Column(Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
+            Column(
+                Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.titleLarge)
             }
-            ElevatedButton(onClick = { extended.value = !extended.value }) {
-                Text(if (extended.value) "Show less" else "Show more")
+            ElevatedButton(onClick = { extended = !extended }) {
+                Text(if (extended) "Show less" else "Show more")
             }
         }
     }
 }
 
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, heightDp = 100, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun DefaultPreview() {
     ComposeTestAppTheme {
         MyApp()
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DefaultPreview2() {
+    ComposeTestAppTheme {
+        Greetings()
     }
 }
--- app/src/main/java/test/app/composetestapp/ui/theme/Theme.kt
+++ app/src/main/java/test/app/composetestapp/ui/theme/Theme.kt
@@ -10,21 +10,36 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
+)
+
+//private val DarkColorScheme = darkColorScheme(
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+//)
+
+//private val LightColorScheme = lightColorScheme(
+//    primary = Purple40,
+//    secondary = PurpleGrey40,
+//    tertiary = Pink40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -35,7 +50,7 @@ private val LightColorScheme = lightColorScheme(
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
     */
-)
+//)
 
 @Composable
 fun ComposeTestAppTheme(

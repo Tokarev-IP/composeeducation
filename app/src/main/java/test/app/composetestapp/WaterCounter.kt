0fd8a76100980a719b2482034920ab1f066da0ca
TokarevIP <tokarevrab@gmail.com>
2023-01-27 21:36:55 +0000

    google compose education part 4

--- app/src/main/java/test/app/composetestapp/WaterCounter.kt
+++ app/src/main/java/test/app/composetestapp/WaterCounter.kt
@@ -1,36 +1,31 @@
-package test.app.composetestapp.examples
+package test.app.composetestapp
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import test.app.composetestapp.WellnessTaskItem
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember {mutableStateOf(0)}
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    taskName = "Have you taken your 15 minute walk today?",
-                    onClose = { showTask = false })
-            }
             Text(text = "You have had $count glasses")
-
         }
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(onClick = { count++ }, enabled = count < 10) {
-                Text("Add one")
-            }
-            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
-                Text("Clear water count")
-            }
+        Button(onClick = onIncrement, enabled = count < 10) {
+            Text("Add one")
         }
     }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier){
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count = count, onIncrement = { count++ })
 }
--- app/src/main/java/test/app/composetestapp/WellnessScreen.kt
+++ app/src/main/java/test/app/composetestapp/WellnessScreen.kt
@@ -1,10 +1,25 @@
 package test.app.composetestapp
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
-import test.app.composetestapp.examples.WaterCounter
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column {
+        StatefulCounter()
+
+        WellnessTasksList(
+            modifier,
+            onCheckedTask = ,
+            onCloseTask = { task -> wellnessViewModel.remove(task) },
+            list = wellnessViewModel.tasks,
+        )
+    }
 }
